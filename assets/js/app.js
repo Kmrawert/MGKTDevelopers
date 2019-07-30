90b5ec8b7bacac0c602371b43485c36e53e8e6d0
trevor orourke <trevororourke8@gmail.com>
2019-07-29 19:20:05 -0500

    my part

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,3 +23,52 @@ $("button").on("click", function() {
     }
 
     searchPop();})
+
+    function getWeather() {
+
+      var location = "chicago"
+    
+      // Constructing a queryURL using the animal name
+      var queryURL = "http://api.apixu.com/v1/current.json?key=713c9ccd7baa43fe92d14242192507&q=" + location
+     
+    
+    
+    
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+        // After data comes back from the request
+        .then(function (response) {
+          console.log(queryURL);
+    
+          var results = response;
+          console.log(results);
+    
+    
+          var locationName = results.location.name
+          var condition = results.condition
+          var temp_f = results.temp_f
+    
+         
+          $("#weather").html(renderMyThing(results))
+    
+    
+          function renderMyThing(response) {
+            var html = `
+              <div id="weather">
+              <h1>${results.location.name} </h1>
+              ${results.current.condition.text} <br>
+              ${results.current.temp_f} <br>
+             
+              <img src="https:${response.current.condition.icon}"> <br>
+            </div>
+        
+              `
+            return html
+          }
+    
+        })
+    
+    }
+    getWeather();
